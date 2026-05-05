475c20a742d9b87c96f81224870a55d35e56219e
Atharva kolte <atharv31kolte@gmail.com>
2026-05-05 21:02:03 +0530

    Refactor BST to AVL tree implementation

--- AVLTree.cpp
+++ AVLTree.cpp
@@ -2,142 +2,116 @@
 using namespace std;
 
 class Node{
-	public:
-		int data;
-		Node* left;
-		Node* right;
-		Node(int val){
-			data=val;
-			left=NULL;
-			right=NULL;
-		}
+    public:
+        int data;
+        Node* left;
+        Node* right;
+        Node(int val){
+            data=val;
+            left=right=NULL;
+        }
 };
 
-class BST{
-	private:
-		Node* head;
-		
-	public:
-		BST(){
-			head=NULL;
-		}
-		
-		Node* add(int value,Node* cur){
-			if(cur==NULL){
-				cur=new Node(value);
-				head=cur;
-				return cur;
-			}
-			if(cur->data<value){
-				cur->right=add(value,cur->right);
-				cur=balance(cur);
-			}
-			else{
-				cur->left=add(value,cur->left);
-				cur=balance(cur);
-			}
-			head=cur;
-			return cur;
-		}
-		
-		int height(Node* cur){
-			if(cur==NULL) return 0;
-			if(cur->left==NULL && cur->right==NULL) return 0;
-			int lh,rh;
-			lh=height(cur->left);
-			rh=height(cur->right);
-			if(lh>rh) return 1+lh;
-			else return 1+rh;
-		}
-		
-		int difference(Node* cur){
-			int lheight=height(cur->left);
-			int rheight=height(cur->right);
-			return lheight-rheight;
-		}
-		
-		Node* balance(Node *cur){
-			int balFactor=difference(cur);
-			if(balFactor>1){
-				if(difference(cur->left)>0) cur=LLrotation(cur);
-				else cur=LRrotation(cur);
-			}
-			else if(balFactor<-1){
-				if(difference(cur->right)<0) cur=RRrotation(cur);
-				else cur=RLrotation(cur);
-			}
-			return cur;
-		}
+class AVL{
+    private:
+        Node* head;
+    public:
+        AVL(){
+            head=NULL;
+        }
+        Node* getroot(){
+            return head;
+        }
 
-		
-		Node* LLrotation(Node *t){
-			Node* leftTree=t->left;
-			t->left=leftTree->right;
-			leftTree->right=t;
-			return leftTree;
-		}
-		
-		Node* RRrotation(Node* t){
-			
-			Node* rightTree=t->right;
-			t->right=rightTree->left;
-			rightTree->left=t;
-			return rightTree;
-		}
-		
-		Node* LRrotation(Node* t){
-			Node* leftTree=t->left;
-			t->left=RRrotation(leftTree);
-			return LLrotation(t);
-		}
-		
-		Node* RLrotation(Node* t){
-			Node* rightTree=t->right;
-			t->right=LLrotation(rightTree);
-			return RRrotation(t);
-		}
-		
-		void display(Node* t){
-			if(t==NULL){
-				return;
-			}
-			display(t->left);
-			cout<<t->data<<" ";
-			display(t->right);
-		}
-	
-		Node* getroot(){
-			return head;
-		}
+        int height(Node* t){
+            if(!t) return 0;
+            int l=height(t->left);
+            int r=height(t->right);
+            return 1+(l>r?l:r);
+        }
+
+        int difference(Node* t){
+            return height(t->left)-height(t->right);
+        }
+
+        Node* LLrotation(Node* t){
+            Node* lefttree=t->left;
+            t->left=lefttree->right;
+            lefttree->right=t;
+            return lefttree;   
+        }
+
+        Node* RRrotation(Node* t){
+            Node* righttree=t->right;
+            t->right=righttree->left;
+            righttree->left=t;
+            return righttree;
+        }
+        
+        Node* LRrotation(Node* t){
+            t=RRrotation(t->left);
+            return LLrotation(t);
+        }
+
+        Node* RLrotation(Node* t){
+            t=LLrotation(t->right);
+            return RRrotation(t);
+        }
+
+        Node* balance(Node* t){
+            int balfactor=difference(t);
+            if(balfactor>1){
+                if(difference(t->left)>0){
+                    t=LLrotation(t);
+                    return t;
+                }
+                else{
+                    t=LRrotation(t);
+                }
+            }
+            else if(balfactor<-1){
+                if(difference(t->right)<0){
+                    t=RRrotation(t);
+                }
+                else{
+                    t=RLrotation(t);
+                }
+            }
+            return t;
+        }
+
+        Node* insert(Node* t,int val){
+            if(t==NULL){
+                t=new Node(val);
+                head=t;
+                return t;
+            }
+            if(t->data>val){
+                t->left=insert(t->left,val);
+                t=balance(t);
+            }
+            else{
+                t->right=insert(t->right,val);
+                t=balance(t);
+            }
+            head=t;
+            return t;
+        }
+
+        void display(Node* t){
+            if(!t) return;
+            cout<<t->data<<" ";
+            display(t->left);
+            display(t->right);
+        }
 
 };
-
 
 int main(){
-	BST obj;
-	obj.add(20,obj.getroot());
-	obj.add(15,obj.getroot());
-	obj.add(25,obj.getroot());
-	//for ll
-	/*obj.add(12);
-	obj.add(18);
-	obj.add(10);*/
-	// for rr
-	/*obj.add(27);
-	obj.add(22);
-	obj.add(29);*/
-	//for lr
-	obj.add(12,obj.getroot());
-	obj.add(18,obj.getroot());
-	obj.add(19,obj.getroot());
-	// for rl
-	/*obj.add(22);
-	obj.add(27);
-	obj.add(21);*/
-	obj.display(obj.getroot());
-	cout<<endl;
-	obj.RLrotation(obj.getroot());
-	obj.display(obj.getroot());
-	cout<<endl;
-	return 0;
+    AVL obj;
+    obj.insert(obj.getroot(),10);
+    obj.insert(obj.getroot(),20);
+    obj.insert(obj.getroot(),15);
+    obj.display(obj.getroot());
+    return 0;
 }
